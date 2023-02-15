e73e300f6767227de5504c9c7abd32941f01afd1
beny201 <artur.wudz@gmail.com>
2023-02-15 21:50:13 +0100

    First part not needed cause counting cover it

--- 1_kata.py
+++ 1_kata.py
@@ -2,9 +2,6 @@
 
 
 def xo(s):
-    #checking if o and x are in str.
-    if s.lower().count("o") == s.lower().count("x") == 0:
-        return True
     #counting 
     if s.lower().count("o") == s.lower().count("x"):
         return True
