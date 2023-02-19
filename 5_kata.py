a073247f008c204f66626ac9c2eccfd4aed3b401
beny201 <artur.wudz@gmail.com>
2023-02-19 21:17:08 +0100

    Improving the style + deleting unnecessary variable

--- 5_kata.py
+++ 5_kata.py
@@ -1,14 +1,11 @@
-#https://www.codewars.com/kata/58f6000bc0ec6451960000fd/train/python
+# https://www.codewars.com/kata/58f6000bc0ec6451960000fd/train/python
 
-def sel_reverse(arr,l):
-    if l>0 and len(arr) > l:
-        qty = len(arr)
-        new_list = [(arr[i:i+l])[::-1] for i in range(0,qty,l)]
-        flat_list = [x for sublist in new_list for x in sublist ]
+def sel_reverse(arr, l):
+    if l > 0 and len(arr) > l:
+        new_list = [(arr[i:i+l])[::-1] for i in range(0, len(arr), l)]
+        flat_list = [x for sublist in new_list for x in sublist]
         return flat_list
-    elif l>0 and len(arr) < l:
+    elif l > 0 and len(arr) < l:
         return arr[::-1]
     else:
         return arr
-    
-
