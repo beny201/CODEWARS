50886dd7a05d20983aedd26601131017cd848815
beny201 <artur.wudz@gmail.com>
2023-02-26 17:36:07 +0100

    Kata level 6

--- 9_kata.py
+++ 9_kata.py
@@ -1,7 +1,9 @@
 # https://www.codewars.com/kata/58235a167a8cb37e1a0000db/train/python
 
 def number_of_pairs(input):
+    # Counting colors
     dic = {}
     for x in input:
         dic[x] = input.count(x)
+    # Searching over dictionary if we have at least 2 same colors
     return sum([int(dic[y] / 2) for y in dic if dic[y] > 1])
