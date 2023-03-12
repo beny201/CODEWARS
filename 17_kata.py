851b52f2f21387b7071bd4a40517f894b912e7fb
beny201 <artur.wudz@gmail.com>
2023-03-12 17:33:50 +0100

    Function to find longest palindrome in string

---
+++ 17_kata.py
@@ -0,0 +1,15 @@
+# https://www.codewars.com/kata/54bb6f887e5a80180900046b/train/python
+
+def longest_palindrome(s):
+    if len(s) > 1:
+        empty_list = []
+        for i in range(len(s)-1):
+            for j in range(len(s)+1):
+                text = s[i:j]
+                if text == text[::-1]:
+                    empty_list.append(text)
+        if len(empty_list) != 0:
+            return max(len(x) for x in empty_list)
+    elif len(s) == 1:
+        return 1
+    return 0git 
